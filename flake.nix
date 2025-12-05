330a71ef5fa1e4f8ff7cba02281d4265bf8553b5
monsterkrampe <monsterkrampe@users.noreply.github.com>
2025-12-05 07:24:48 +0100

    Upgrade flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Basic Lean Datastructures";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
